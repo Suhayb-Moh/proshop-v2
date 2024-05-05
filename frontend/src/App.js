822986f0b40eb532bfd966f4611ff18a76c5892e
Suhayb-Moh <suhaybmohamed@gmail.com>
2024-05-05 17:10:39 +0300

    added homescreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
     <>
       <Header />
       <main className="py-3">
-        <h1>welcome to ProShop</h1>
+        <Container>
+          <HomeScreen />
+        </Container>
       </main>
       <Footer />
     </>
