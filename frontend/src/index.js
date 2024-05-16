18c32bc777345fd0ae8c63b61756a697cdeacdef
Suhayb-Moh <suhaybmohamed@gmail.com>
2024-05-16 14:38:58 +0300

    added shipping and payment page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,6 +18,9 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreeen from "./screens/CartScreeen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PrivateRoute from "./components/PrivateRoute";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,6 +30,10 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreeen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+        <Route path="/payment" element={<PaymentScreen />} />
+      </Route>
     </Route>
   )
 );
